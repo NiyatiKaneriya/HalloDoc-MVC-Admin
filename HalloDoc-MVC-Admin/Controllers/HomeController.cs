2036af6b663180d8701f7d1201b132498dc313c4
NiyatiKaneriya <niyati.kaneriya@etatvasoft.com>
2024-02-19 18:23:38 +0530

    Add project files.

---
+++ HalloDoc-MVC-Admin/Controllers/HomeController.cs
@@ -0,0 +1,24 @@
+﻿using HalloDoc_MVC_Admin.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HalloDoc_MVC_Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HalloDoc-MVC-Admin/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDoc_MVC_Admin.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+    }
+}
